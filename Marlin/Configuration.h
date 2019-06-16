360fbebee18c77e980e5568976a550835b14fbce
spm <jan.krajdl@cecolo.com>
2019-06-16 20:08:45 +0200

    Changed extruder settings (steps, direction)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 404, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 404, 406 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -856,7 +856,7 @@
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
 #define INVERT_E0_DIR false
-#define INVERT_E1_DIR false
+#define INVERT_E1_DIR true
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
 #define INVERT_E4_DIR false
