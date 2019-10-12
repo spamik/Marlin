5fe6100df63993f0ef97b8afa60210e85c28a16e
spm <jan.krajdl@cecolo.com>
2019-10-13 01:10:26 +0200

    Changed new software endstops

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -879,7 +879,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
+#define X_BED_SIZE 250
 #define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
@@ -888,7 +888,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 190
 
 /**
  * Software Endstops
