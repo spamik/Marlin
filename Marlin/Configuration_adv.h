879c509879174bb5d3696f052124497029fac4b8
Jan Krajdl <spm@spamik.cz>
2019-11-25 01:30:21 +0100

    Decreased trinamic drivers current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1102,28 +1102,28 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT          500  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          800
+  #define Y_CURRENT          500
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT          800
+  #define Z_CURRENT          500
   #define Z_MICROSTEPS        16
 
-  #define X2_CURRENT         800
+  #define X2_CURRENT         500
   #define X2_MICROSTEPS       16
 
-  #define Y2_CURRENT         800
+  #define Y2_CURRENT         500
   #define Y2_MICROSTEPS       16
 
-  #define Z2_CURRENT         800
+  #define Z2_CURRENT         500
   #define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         600
+  #define E0_CURRENT         450
   #define E0_MICROSTEPS       16
 
-  #define E1_CURRENT         600
+  #define E1_CURRENT         450
   #define E1_MICROSTEPS       16
 
   #define E2_CURRENT         800
