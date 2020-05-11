0600db39871bb420edeb367d8d4a3b2975409ff5
SPM <spm@spamik.cz>
2020-05-11 02:16:27 +0200

    Added double filament sensor

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1007,7 +1007,7 @@
                                                   // Filament Unload does a Retract, Delay, and Purge first:
   #define FILAMENT_UNLOAD_RETRACT_LENGTH      13  // (mm) Unload initial retract length.
   #define FILAMENT_UNLOAD_DELAY             5000  // (ms) Delay for the filament to cool after retract.
-  #define FILAMENT_UNLOAD_PURGE_LENGTH         8  // (mm) An unretract is done, then this length is purged.
+  #define FILAMENT_UNLOAD_PURGE_LENGTH         100  // (mm) An unretract is done, then this length is purged.
 
   #define PAUSE_PARK_NOZZLE_TIMEOUT           120  // (seconds) Time limit before the nozzle is turned off for safety.
   #define FILAMENT_CHANGE_ALERT_BEEPS         10  // Number of alert beeps to play when a response is needed.
--- Marlin/pins_RUMBA.h
+++ Marlin/pins_RUMBA.h
@@ -46,9 +46,9 @@
 #define X_MIN_PIN          37
 #define X_MAX_PIN          36
 #define Y_MIN_PIN          35
-#define Y_MAX_PIN          34
+//#define Y_MAX_PIN          34
 #define Z_MIN_PIN          33
-#define Z_MAX_PIN          32
+//#define Z_MAX_PIN          32
 
 //
 // Z Probe (when not Z_MIN_PIN)
@@ -182,3 +182,7 @@
   #define LCD_PINS_D5      38
   #define LCD_PINS_D6      41
 #endif
+
+//#define FIL_RUNOUT_PIN Y_MAX_PIN
+#define FIL_RUNOUT_PIN 34
+#define FIL_RUNOUT2_PIN 32
